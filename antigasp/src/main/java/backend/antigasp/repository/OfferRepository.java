4f82d03630cefa5e99eaffcd6232b2dfd4777ddd
gaspardarosafrancisco@gmail.com <gaspardarosafrancisco@gmail.com>
2026-05-11 20:32:31 +0200

    feat: implement repositories

---
+++ antigasp/src/main/java/backend/antigasp/repository/OfferRepository.java
@@ -0,0 +1,14 @@
+package backend.antigasp.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import backend.antigasp.entity.Offer;
+
+@Repository
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+	List<Offer> findByMerchantId(Long merchantId);
+}
---
+++ antigasp/src/main/java/backend/antigasp/repository/ReservationRepository.java
@@ -0,0 +1,15 @@
+package backend.antigasp.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import backend.antigasp.entity.Reservation;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long>{
+
+	List<Reservation> findByUserId(Long userId);
+    List<Reservation> findByOfferId(Long offerId);
+}
